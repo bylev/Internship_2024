f4b4f5e5a508a681e8d04c9610bfcc6315d66e8d
Michelle Cámara <84045565+bylev@users.noreply.github.com>
2024-01-05 19:07:31 -0600

    Test 01_HelloWorld

---
+++ Host/Test01.c
@@ -0,0 +1,18 @@
+/*
+ * Test01.c
+ *
+ *  Created on: Jan 3, 2024
+ *      Author: Michelle Cámara
+ */
+
+
+#include <stdio.h>
+
+int main(void){
+	printf("Hello \n");
+	printf("PRESS 'ENTER' to exit");
+
+	getchar();
+
+	return 0;
+}
