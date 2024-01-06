6521dd708b9d9684b4e0e9a1f9ad267cf1ef3e1e
bylev <2009014@upy.edu.mx>
2024-01-05 18:53:27 -0600

    01_StorageClasses Static

---
+++ Static.c
@@ -0,0 +1,29 @@
+/*
+ * StorageClasses.c
+ *
+ *  Created on: Jan 5, 2024
+ *      Author: Michelle Cámara
+ */
+
+
+#include <stdio.h>
+
+
+extern void increment(); // Function declaration to use later
+
+
+int main(void) {
+    increment(); //increment b
+    increment(); //increment b
+    increment(); //increment b
+    return 0;
+}
+int a = 10;
+void increment() {
+    static int b = 3; //defining my static variable
+    printf("Value of my static variable: %d \n", b); //print my actual variable
+    b++; //increment 1
+    getchar();
+}
+
+
